d7367ada0799979982207e8eae0c353ba0b6af4d
maximIs-88 <isachenkomax@gmail.com>
2016-01-03 14:42:23 +0100

    init solution

---
+++ EasyTracker.Business.Entities/TaskCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyTracker.Business.Entities
+{
+    public static class TaskCreator
+    {
+        public static BaseEnitity CreateTask()
+        {
+            return new BaseEnitity
+            {
+                Id = 1,
+                Author = "Max",
+                Description = "New desc",
+                Name = "New task"
+            };
+        }
+    }
+}
---
+++ EasyTracker.UI.Web/Controllers/BacklogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EasyTracker.UI.Web.Controllers
+{
+    public class BacklogController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+//            EasyTracker.Business.Entities.
+            return View();
+        }
+    }
+}
