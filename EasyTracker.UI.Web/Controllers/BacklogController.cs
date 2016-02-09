5934e99a022cb03cd5802ae4efe1b8161bef4e99
maximIs-88 <isachenkomax@gmail.com>
2016-02-09 18:55:24 +0100

    dd

---
+++ EasyTracker.UI.Web/Controllers/BacklogController.cs
@@ -0,0 +1,21 @@
+﻿using EasyTracker.Business.Entities;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyTracker.UI.Web.Controllers
+{
+    public class BacklogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bug = TaskCreator.CreateBug();
+
+            ViewData["Message"] = "Show default bug info";
+
+            return View(bug);
+        }
+    }
+}
--- EasyTracker.UI.Web/Controllers/HomeController.cs
+++ EasyTracker.UI.Web/Controllers/HomeController.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Mvc;
 
 namespace EasyTracker.UI.Web.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
